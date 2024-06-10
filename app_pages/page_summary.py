b6f9f1df19d2085dd28a46bd7bd122e674cf056a
Philip sam <Philipsamaras@gmail.com>
2024-06-10 12:47:54 +0000

    Working on streamlit app

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -29,7 +29,6 @@ def page_summary_body():
 
     st.success(
         f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in having a study to differentiate "
-        f"a parasitized and uninfected cell visually.\n"
-        f"* 2 - The client is interested in telling whether a given cell contains a malaria parasite or not. "
+        f"* 1 - The client is interested in conducting a study to visually differentiate a cherry leaf that is healthy from one that contains powdery mildew.\n"
+        f"* 2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew. "
         )
